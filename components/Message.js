652d049f9c1e3ae9503e93215793d5015b59a808
Horace <hiuhongyung@gmail.com>
2021-03-26 23:12:51 +0800

    Last modified

--- components/Message.js
+++ components/Message.js
@@ -1,9 +1,18 @@
+import { useAuthState } from 'react-firebase-hooks/auth';
 import styled from 'styled-components';
+import {auth} from "../firebase";
+import moment from 'moment';
 
 function Message({user,message}) {
+    const [userLoggedIn] = useAuthState(auth);
+    const TypeOfMessage = user === userLoggedIn.email ? Sender : Reciever;
     return (
         <Container>
-            <p>{message.message}</p>
+            <TypeOfMessage>{message.message}
+                <TimeStamp>
+                {message.timestamp ? moment(message.timestamp).format('LT') : "..." }
+                </TimeStamp>    
+            </TypeOfMessage>
         </Container>
     )
 }
@@ -11,3 +20,27 @@ function Message({user,message}) {
 export default Message
 
 const Container = styled.div``;
+
+const MessageElement = styled.p`
+    width: fit-content;
+    padding: 20px;
+    border-radius: 10px;
+    min-width: 60px;
+    padding-bottom: 22px;
+    position: relative;
+    text-align: right;
+`;
+
+const Sender = styled(MessageElement)`
+margin-left: auto;
+background-color: #dcf8c6;
+`
+
+const Reciever = styled(MessageElement)`
+   background-color: whitesmoke;
+   text-align: left;
+`
+
+const TimeStamp = styled.div`
+    font-size: 10px;
+`;
